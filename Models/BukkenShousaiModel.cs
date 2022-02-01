7b7a67fb076714ff6c401c2e95fdb000bb4f8615
SaeAkao <red.rabbit.sk@gmail.com>
2022-02-01 09:21:05 +0900

    Sae Akao "BukkenShousaiEntry" is under development.

--- Models/BukkenShousaiModel.cs
+++ Models/BukkenShousaiModel.cs
@@ -11,17 +11,27 @@ namespace Models
         public string BukkenNO { get; set; }
         public string SitenCD { get; set; }
         public string TantouEigyouCD { get; set; }
-        public string TantouCadCD { get; set; }
-        public string PCSupportCD { get; set; }
-        public string KubunCD { get; set; }
         public string BukkenName { get; set; }
-        public string KoumutenName { get; set; }
+        public string PCSupportCD { get; set; }
+        public string Address { get; set; }
+        public string TantouCadCD { get; set; }
+        public string KoumutenCD { get; set; }
+        public string NyuuryokusakiCD { get; set; }
+        public decimal? KakouTubosuu { get; set; }
         public int TokuchuuzaiUmu { get; set; }
+        public int NoukiMiteiCheck { get; set; }
+        public string Nouki { get; set; }
+        public string ZairyouNouki { get; set; }
+        public string UnsouSoukoiribi { get; set; }
+        public string TokuchuuzaiComment { get; set; }
+        public string KubunCD { get; set; }
         public IEnumerable<DropDownListItem> SitenSelectList { get; set; }
         public IEnumerable<DropDownListItem> EigyouStaffSelectList { get; set; }
         public IEnumerable<DropDownListItem> PCSupportSelectList { get; set; }
         public IEnumerable<DropDownListItem> CADStaffSelectList { get; set; }
-        public IEnumerable<DropDownListItem> KubunSelectList { get; set; }
+        public IEnumerable<DropDownListItem> KoumutenSelectList { get; set; }
+        public IEnumerable<DropDownListItem> NyuuryokusakiSelectList { get; set; }
         public IEnumerable<DropDownListItem> TokuchuuzaiUmuSelectList { get; set; }
+        public IEnumerable<DropDownListItem> KubunSelectList { get; set; }
     }
 }
