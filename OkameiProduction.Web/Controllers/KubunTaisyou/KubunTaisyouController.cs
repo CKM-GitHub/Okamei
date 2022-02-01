b0ac78471dd4882660316c13105b00f1650e8d5e
capital-phyoethurakyaw <capital.phyoethurakyaw@gmail.com>
2022-02-01 13:43:27 +0630

    PTK

---
+++ OkameiProduction.Web/Controllers/KubunTaisyou/KubunTaisyouController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using Models;
+using OkameiProduction.BL;
+
+namespace OkameiProduction.Web.Controllers.KubunTaisyou
+{
+    public class KubunTaisyouController : BaseController
+    {
+        // GET: Classification
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Classification()
+        {
+            var vm = new KubunTaisyouModel();
+            SetDropDownListItems(vm);
+
+            ViewBag.ServerDate = DateTime.Now.ToString(DateTimeFormat.yyyyMMdd);
+
+            return View(vm); 
+        }
+        private void SetDropDownListItems(KubunTaisyouModel vm)
+        {
+            CommonBL dl = new CommonBL();
+            vm.SitenSelectList = dl.GetMultiPorposeDDLItems(EMultiPorpose.Siten);
+            vm.KubunSelectList = dl.GetMultiPorposeDDLItems(EMultiPorpose.Kubun);
+        }
+        // GET: 
+        public ActionResult DisplayResult()
+        {
+            var vm = GetFromQueryString<KubunTaisyouModel>();
+
+            var bl = new KubunTaisyouBL();
+            var dt = bl.GetDisplayResult(vm);
+            ViewBag.Data = dt;
+
+            return View(vm);
+        }
+    }
+}
