a84b51e4967de3f99bdd317bd92bb2e10991791c
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-02-01 13:54:54 +0630

    tza

---
+++ OkameiProduction.Web/Controllers/MoulderItiran/MoulderItiranController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using Models;
+using OkameiProduction.BL;
+
+namespace OkameiProduction.Web.Controllers
+{
+    public class MoulderItiranController : BaseController
+    {
+        // GET: 
+        public ActionResult SetCondition()
+        {
+            var vm = new MoulderItiranModel();
+            SetDropDownListItems(vm);
+
+            ViewBag.ServerDate = DateTime.Now.ToString(DateTimeFormat.yyyyMMdd);
+
+            return View(vm);
+        }
+
+        // GET: 
+        public ActionResult DisplayResult ()
+        {
+            var vm = GetFromQueryString<MoulderItiranModel>();
+
+            MoulderItiranBL bl = new MoulderItiranBL();
+            var dt = bl.GetDisplayResult(vm);
+            ViewBag.Data = dt;
+
+            return View(vm);
+        }
+
+
+
+
+        // ----------------------------------------/
+        // private
+        private void SetDropDownListItems(MoulderItiranModel vm)
+        {
+            CommonBL dl = new CommonBL();
+            vm.SitenSelectList = dl.GetMultiPorposeDDLItems(EMultiPorpose.Siten);
+        }
+    }
+}
