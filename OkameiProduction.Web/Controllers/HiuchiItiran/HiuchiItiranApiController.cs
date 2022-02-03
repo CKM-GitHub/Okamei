93f8f7679b01f6fd021ad9c8cfc356e38e761f08
capital-phyoethurakyaw <capital.phyoethurakyaw@gmail.com>
2022-02-03 13:19:45 +0630

    PTK

---
+++ OkameiProduction.Web/Controllers/HiuchiItiran/HiuchiItiranApiController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http;
+using Models;
+using OkameiProduction.BL;
+
+namespace OkameiProduction.Web.Controllers
+{
+    public class HiuchiItiranApiController : BaseApiController
+    {
+
+    }
+}
