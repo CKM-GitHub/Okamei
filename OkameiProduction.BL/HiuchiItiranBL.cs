1313acfd6c2f66574402f0baf6b926e7be3e5231
capital-phyoethurakyaw <capital.phyoethurakyaw@gmail.com>
2022-02-04 18:41:59 +0630

    PTK

--- OkameiProduction.BL/HiuchiItiranBL.cs
+++ OkameiProduction.BL/HiuchiItiranBL.cs
@@ -11,29 +11,21 @@ namespace OkameiProduction.BL
 
         public DataTable GetDisplayResult(HiuchiItiranModel model)
         {
-            SqlParameter[] sqlParams = new SqlParameter[6];
+            SqlParameter[] sqlParams = new SqlParameter[5];
             sqlParams[0] = new SqlParameter("@TantouSitenCD", SqlDbType.VarChar) { Value = model.SitenCD.ToStringOrNull() };
             sqlParams[1] = new SqlParameter("@NoukiStart", SqlDbType.Date) { Value = model.NoukiStart.ToDateTime() };
             sqlParams[2] = new SqlParameter("@NoukiEnd", SqlDbType.Date) { Value = model.NoukiEnd.ToDateTime() }; 
             sqlParams[3] = new SqlParameter("@TantouEigyouCD", SqlDbType.VarChar) { Value = model.TantouEigyouCD.ToStringOrNull() }; 
             sqlParams[4] = new SqlParameter("@TantouCadCD", SqlDbType.VarChar) { Value = model.TantouCadCD.ToStringOrNull() };
-            sqlParams[5] = new SqlParameter("@UserID", SqlDbType.VarChar) { Value = model.UserID.ToStringOrNull() };
+            //sqlParams[5] = new SqlParameter("@UserID", SqlDbType.VarChar) { Value = model.UserID.ToStringOrNull() };
 
 
             DBAccess db = new DBAccess();
-            var dt = db.SelectDatatable("KubunTaisyou_SelectDisplayResult", sqlParams);
-
-            if (model.SortOption == 2)
-            {
-                var query = from dr in dt.AsEnumerable()
-                            orderby dr.Field<string>("KoumutenName"), dr.Field<string>("Nouki")
-                            select dr;
-                return query.CopyToDataTable();
-            }
-            else
-            {
-                return dt;
-            }
+            var dt = db.SelectDatatable("HiuchiItiran_SelectDisplayResult", sqlParams);
+            dt.Columns.Remove("Flg");
+            dt.Columns.Add("Export");
+            dt.AcceptChanges();
+            return dt; 
         }
     }
 }
