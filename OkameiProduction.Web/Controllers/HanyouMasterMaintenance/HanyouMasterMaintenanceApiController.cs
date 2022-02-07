17eafd28d36eb3992bb821762691e21f191e3f92
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-02-07 18:34:45 +0630

    tza for HanyouMasterMaintenance

---
+++ OkameiProduction.Web/Controllers/HanyouMasterMaintenance/HanyouMasterMaintenanceApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using Models;
+using OkameiProduction.BL;
+
+namespace OkameiProduction.Web.Controllers
+{
+    public class HanyouMasterMaintenanceApiController : BaseApiController
+    {
+        [HttpPost]
+        public string ExistsDisplayResult([FromBody] BukkenItiranModel model)
+        {
+            if (model == null) return GetBadRequestResult();
+
+            var bl = new BukkenItiranBL();
+            if (bl.ExistsDisplayResult(model))
+            {
+                return GetSuccessResult();
+            }
+            else
+            {
+                return GetErrorResult("S013");
+            }
+        }
+    }
+}
---
+++ OkameiProduction.Web/Controllers/HanyouMasterMaintenance/HanyouMasterMaintenanceController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Mvc;
+using Models;
+using OkameiProduction.BL;
+
+namespace OkameiProduction.Web.Controllers
+{
+    public class HanyouMasterMaintenanceController : BaseController
+    {
+        // GET: 
+        public ActionResult SetCondition()
+        {
+            var vm = new BukkenItiranModel();
+            //SetDropDownListItems(vm);
+
+            ViewBag.ServerDate = DateTime.Now.ToString(DateTimeFormat.yyyyMMdd);
+
+            return View(vm);
+        }
+
+        // GET: 
+        public ActionResult DisplayResult ()
+        {
+            var vm = GetFromQueryString<BukkenItiranModel>();
+
+            BukkenItiranBL bl = new BukkenItiranBL();
+            var dt = bl.GetDisplayResult(vm);
+            ViewBag.Data = dt;
+
+            return View(vm);
+        }
+
+
+
+
+        // ----------------------------------------/
+        // private
+        private void SetDropDownListItems(BukkenItiranModel vm)
+        {
+            CommonBL dl = new CommonBL();
+            vm.TantouSitenDropDownListItems = dl.GetMultiPorposeDropDownListItems(EMultiPorpose.TantouSiten);
+            vm.TantouEigyouDropDownListItems = dl.GetMultiPorposeDropDownListItems(EMultiPorpose.TantouEigyou);
+            vm.TantouPcDropDownListItems = dl.GetMultiPorposeDropDownListItems(EMultiPorpose.TantouPc);
+            vm.TantouCadDropDownListItems = dl.GetMultiPorposeDropDownListItems(EMultiPorpose.TantouCad);
+            vm.KubunDropDownListItems = dl.GetMultiPorposeDropDownListItems(EMultiPorpose.Kubun);
+            vm.TokuchuuzaiUmuDropDownListItems = dl.GetTokuchuuzaiUmuDropDownListItems();
+        }
+    }
+}
