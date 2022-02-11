0b80cb4821661b28b50e6e9d3315ab6c0abd1ef4
capital-poeeisan <capital.poeeisan@gmail.com>
2022-02-11 10:30:28 +0630

    pes

---
+++ OkameiProduction.BL/InputStepBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Models;
+
+namespace OkameiProduction.BL
+{
+    public class InputStepBL
+    {
+        public bool ExistsDisplayResult(InputStepModel model)
+        {
+            SqlParameter[] sqlParams = new SqlParameter[1];
+           
+            sqlParams[0] = new SqlParameter("@TantouCadCD", SqlDbType.VarChar) { Value = model.TantouCadCD.ToStringOrNull() };
+           
+          
+
+            DBAccess db = new DBAccess();
+            var dt = db.SelectDatatable("InputStep_SelectDisplayResult", sqlParams);
+            return dt.Rows.Count > 0;
+        }
+        public DataTable GetDisplayResult(InputStepModel model)
+        {
+            SqlParameter[] sqlParams = new SqlParameter[1];
+            DBAccess db = new DBAccess();
+            var dt = db.SelectDatatable("InputStep_SelectDisplayResult", sqlParams);
+            return dt;
+            
+        }
+
+    }
+}
---
+++ OkameiProduction.Web/Controllers/InputStep/InputStepApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using Models;
+using OkameiProduction.BL;
+
+namespace OkameiProduction.Web.Controllers
+{
+    public class InputStepApiController : BaseApiController
+    {
+        [HttpPost]
+        public string ExistsDisplayResult([FromBody] InputStepModel model)
+        {
+            if (model == null) return GetBadRequestResult();
+
+            var bl = new InputStepBL();
+            if (bl.ExistsDisplayResult(model))
+            {
+                return GetSuccessResult();
+            }
+            else
+            {
+                return GetErrorResult("S013");
+            }
+        }
+    }
+}
