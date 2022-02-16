0e6b7dc8f596bb3b3c8cbbc5d4fc8fa760318599
Than Zaw Aung <capital.thanzawaung@gmail.com>
2022-02-16 14:42:47 +0630

    tza for SMitumoriKanri

---
+++ Models/SmitumoriKanriModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class SmitumoriKanriModel : BaseModel
+    {
+        public string sagyou_sDate { get; set; }
+        public string sagyou_eDate { get; set; }
+        public IEnumerable<DropDownListItem> TantouSitenDropDownListItems { get; set; }
+        public IEnumerable<DropDownListItem> TantouEigyouDropDownListItems { get; set; }
+        public IEnumerable<DropDownListItem> TantouPcDropDownListItems { get; set; }
+        public IEnumerable<DropDownListItem> TantouCadDropDownListItems { get; set; }
+        public IEnumerable<DropDownListItem> KubunDropDownListItems { get; set; }
+        public IEnumerable<DropDownListItem> TokuchuuzaiUmuDropDownListItems { get; set; }
+    }
+}
